e49f6248eccf296566f0ac24b790f9dea1ebb0da
Senith Kuruvitage <senith.p.m.kuruvitage20@gmail.com>
2026-07-16 00:58:16 +0530

    1 - 5
    Added comments and improved formatting.

--- 1.     C Programming Fundamentals/1.c
+++ 1.     C Programming Fundamentals/1.c
@@ -2,10 +2,10 @@
    Date        : June 15, 2026
    Program     : prints "Hello World!"  */
 
-#include<stdio.h>
+#include<stdio.h> // Standard input/output library
 int main() 
-{
+{   
 	printf("Hello World!\n");
 
 	return 0;
-} 
+} // End of main function.
