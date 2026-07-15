dabb7c2b109d43d018f8d6fde2c928840666a4ae
Senith Kuruvitage <senith.p.m.kuruvitage20@gmail.com>
2026-07-15 22:49:27 +0530

    Add files via upload

---
+++ 1.     C Programming Fundamentals/one.c
@@ -0,0 +1,11 @@
+/* Auther name : Senith Kuruvitage
+   Date        : June 15, 2026
+   Program     : prints "Hello World!"  */
+
+#include<stdio.h>
+int main() 
+{
+	printf("Hello World!\n");
+
+	return 0;
+} 
