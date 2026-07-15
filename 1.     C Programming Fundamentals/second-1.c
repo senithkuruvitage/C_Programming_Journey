97b47e46b9d4f34b07184de151d5f180c95424cb
Senith Kuruvitage <senith.p.m.kuruvitage20@gmail.com>
2026-07-16 01:07:12 +0530

    Add files via upload

---
+++ 1.     C Programming Fundamentals/second-1.c
@@ -0,0 +1,26 @@
+// LAB - 02 _ Exercise - 01
+/* Lab 2, Exercise 1 consists of three parts.
+   The First part displays the text "Hello! Welcome to SLIIT")
+   The Second part demonstrates the use of the \n (new line) escape sequence.
+   The Third part displays a pattern created using the letter X. */
+
+#include<stdio.h>
+//function main begins program execution
+int main()
+
+{
+	printf("\nHello! Welcome to SLIIT\n\n");                 // 2.1.1
+
+	printf("First Line\nThis is the second line\n\n\n");   // 2.1.2
+	
+	printf("       x      \n");                            // 2.1.3
+	printf("     x   x    \n");
+	printf("   x       x  \n");
+	printf("     x   x    \n");
+	printf("   x       x  \n");
+	printf("     x   x    \n");
+	printf("       x      \n");
+     
+   return 0;
+
+} // end of function main
